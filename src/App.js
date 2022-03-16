e3e160dd9ce696f80f72ee676a58cf1bd471676a
dhakalsuman83 <dhakalsuman83@gmail.com>
2022-03-17 10:12:05 +1100

    Worked with displaying the tasks and deleting the task using the state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import { useState } from "react";
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
-  const name = "Suman";
-  const x = false;
+  const [tasks,setTasks]=useState([
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: "Feb 5th at 2:30PM",
+    remainder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting at school",
+    day: "Feb 6th at 1:30PM",
+    remainder: true,
+  },
+  {
+    id: 3,
+    text: "Food Shopping",
+    day: "Feb 5th at 3:30PM",
+    remainder: false,
+  },
+  ])
+  //delete Task
+  const deleteTask = (id) => {
+   setTasks(tasks.filter(task=> task.id !=id))
+  }
+  
+  //Toggle Remainder
   return (
     <div className="container">
       <Header />
+      {(tasks.length > 0) ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+          "No task to Show"
+      )
+      }
     </div>
   );
 }
