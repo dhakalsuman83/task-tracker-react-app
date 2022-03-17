4c895ad56f1bbe089c1ed71c7705c67fcd9257ac
dhakalsuman83 <dhakalsuman83@gmail.com>
2022-03-17 13:03:46 +1100

    worked in the backend using the json-server and completed the app without routing

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+      <div>
+          <h4>Created by: Suman Dhakal</h4>
+          <h4>Learned from Travese Media</h4>
+          <a href="/">Go Back to main page</a>
+    </div>
+  )
+}
+
+export default About
